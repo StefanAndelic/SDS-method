1f588233c3f0dbe87fb7766e5d42ea56de725856
StefanAndelic <70983054+StefanAndelic@users.noreply.github.com>
2022-02-04 13:00:58 -0500

    changes

--- models/team_model.js
+++ models/team_model.js
@@ -31,6 +31,12 @@ const Team = mongoose.model(
       minlength: 5,
       maxlength: 255,
     },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 1024,
+    },
   })
 );
 
@@ -43,6 +49,7 @@ function validateMember(member) {
     name: Joi.string().min(5).max(50),
     role: Joi.string().min(5).max(50),
     email: Joi.string().min(5).max(50),
+    password: Joi.string().min(5).max(255).required(),
   });
   return schema.validate(member);
 }
