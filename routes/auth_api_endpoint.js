07c16e5b9d599a15692d7b35348d78ef3415695b
StefanAndelic <70983054+StefanAndelic@users.noreply.github.com>
2022-02-04 13:04:13 -0500

    auth endpoint

---
+++ routes/auth_api_endpoint.js
@@ -0,0 +1,40 @@
+//utils
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//model
+const { Team } = require("../models/team_model");
+
+const router = express.Router();
+
+/*
+ * authenticates the member
+ */
+router.post("/", async (req, res) => {
+  let member = await Team.findOne({ email: req.body.email });
+  if (!member) return res.status(400).send("Invalid email ");
+  const validPassword = await bcrypt.compare(
+    req.body.password,
+    member.password
+  );
+  if (!validPassword) return res.status(400).send("Invalid password");
+
+  //generate the token
+  const token = jwt.sign(
+    {
+      id: member.id,
+      name: member.name,
+      email: member.email,
+      roles: member.roles,
+    },
+
+    "jwtPrivateKey",
+    { expiresIn: 60 * 60 }
+  );
+
+  //send response
+  res.send(token);
+});
+
+module.exports = router;
