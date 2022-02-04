43d8afcec3dc14ec0520d281b1dbc046e7f9f47f
StefanAndelic <70983054+StefanAndelic@users.noreply.github.com>
2022-02-04 13:05:25 -0500

    changes

--- seed/team.js
+++ seed/team.js
@@ -8,34 +8,38 @@ const data = [
     name: "test1",
     email: "test1@ibm.com",
     role: "Project Manager",
+    password: "test1#",
   },
   {
     id: "2",
     name: "test2",
     email: "test2@ibm.com",
     role: "Backend Engineer",
+    password: "test2#",
   },
   {
     id: "3",
     name: "test3",
     email: "test3@ibm.com",
     role: "DevOps Engineer",
+    password: "test3#",
   },
   {
     id: "4",
     name: "test4",
     email: "tes4@ibm.com",
     role: "UX Design",
+    password: "test4#",
   },
 ];
 
 function connectDB() {
   mongoose
-    .connect(`mongodb://localhost/${db.testing_db2}`, {
+    .connect(`mongodb://localhost/${db.testing_db}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => console.log(`Connected to ${db.testing_db2} database`))
+    .then(() => console.log(`Connected to ${db.testing_db} database`))
     .catch((err) => error("Could not connect to DB...Please try again"));
 }
 
@@ -50,6 +54,7 @@ async function seed() {
       name: member.name,
       email: member.email,
       role: member.role,
+      password: member.password,
     }).save();
 
     await Team.insertMany(member);
