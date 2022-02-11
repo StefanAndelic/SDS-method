720460b9fe47ee7ff8b027b4cdda6d35ce7f36e9
Jerry D Kindig <Jerry.Kindig@ibm.com>
2022-02-11 08:33:39 -0500

    Added Project model, post + delete endpoints

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const staging = require("./config/staging_env.json");
 
 const team = require("./routes/team_api_endpoint");
-const auth = require("./routes/auth_api_endpoint");
+const project = require("./routes/project_api_endpoint");
 
 const app = express();
 app.use(express.json());
 
 app.use("/ibm/api/v1/team", team);
-
-app.use("/ibm/api/v1/auth", auth);
+app.use("/ibm/api/v1/project", project);
 
 //connects to the database
 require("./connection/db_connection")();
--- routes/team_api_endpoint.js
+++ routes/team_api_endpoint.js
@@ -1,5 +1,7 @@
 //utils
 const express = require("express");
+const { eq } = require("lodash");
+const { Mongoose } = require("mongoose");
 
 //model
 const { Team } = require("../models/team_model");
@@ -14,6 +16,44 @@ router.get("/", async (req, res) => {
   if (!member) return res.status(404).send("Team members could not be found");
 
   res.send(member);
+});
+
+/*
+ * posts a user to the db
+ */
+router.post("/", (req, res, next) => {
+  const member = new Team({
+    id: req.body.id,
+    name: req.body.name,
+    role: req.body.role,
+    email: req.body.email
+  });
+  member.save().then(result =>{
+    console.log(result)
+  })
+  .catch(err => console.log(err));
+  res.status(201).json({
+    message: 'Handling POST requests to /team',
+    createdMember: member
+  })
+});
+
+/*
+ * deletes a user from the db
+ */
+router.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+  Team.deleteMany({id: id })
+  .exec()
+  .then(result => {
+    res.status(200).json(result);
+  })
+  .catch(err =>{
+    console.log(err)
+    res.status(500).json({
+      error: err
+    })
+  })
 });
 
 module.exports = router;
